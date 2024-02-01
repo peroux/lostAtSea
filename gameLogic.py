07a5217dfe4851db2cc49238f53813d2c37ec4ad
peroux <91110219+peroux@users.noreply.github.com>
2024-02-01 11:55:09 -0600

    Update gameLogic.py

--- gameLogic.py
+++ gameLogic.py
@@ -1,4 +1,10 @@
+import random
+import os
+import platform
+
 playerScore = 0
+playerRankings = []
+remainingItems = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 costGuardRankings = [
     [1, "Shaving mirror", "Of all the items, the mirror is absolutely critical. It is the most powerful tool you have for communicating your presence. In sunlight, a simple mirror can generate five to seven million candlepower of light. The reflected sunbeam can even be seen beyond the horizon."],
     [2, "A 10-liter can of oil/gasoline mixture", "The second most critical item for signaling. The mixture will float on water and can be ignited using the matches."],
@@ -16,10 +22,64 @@ costGuardRankings = [
     [14, "A quantity of mosquito netting", "There are NO mosquitoes in the middle of the Atlantic Ocean and the netting is useless for anything else."],
     [15, "A sextant", "Useless without the relevant tables and a chronometer."]
     ]
+
+def clearScreen():
+    # Check if the operating system is Windows
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def runGame():
+    clearScreen()
+    print("Welcome to the Cost Guard Game!")
+    print("You are stranded in the middle of the Atlantic Ocean after a shipwreck.")
+    print("You are in a lifeboat with 11 other people.")
+    print("You have a limited amount of supplies and must rank them in order of importance.")
+    print("You will be scored based on how close your ranking is to the official coast guard ranking.")
+    print("The lower your score, the better.")
+    print("Good luck!")
+    input("Press enter to continue...")
+    rankItems()
+    
+
 def listObjectRanks():
+    clearScreen()
     for item in costGuardRankings:
         print("Ranking: " + str(item[0]) + "\nItem: " + item[1] + "\nReasoning: " + item[2])
         print("----------------------------------------")
-listObjectRanks()
-def GetRankings():
-    return
+
+def scoreUserRankings():
+    for item in playerRankings:
+        playerScore += abs(int(item)-costGuardRankings[playerRankings.index(item)][0])
+    print("Your score is: " + str(playerScore))
+
+def displayCurrentItems():
+    print("Remaining Items: ")
+    random.shuffle(remainingItems)  # Shuffle the list of remaining items
+    for item in remainingItems:
+        print(costGuardRankings[item-1][1])
+
+def rankItems():
+    clearScreen()
+    print("--Select the item you would like to rank--")
+    displayCurrentItems()
+    item = input()
+    if item in costGuardRankings[][1]:
+        clearScreen()
+        print("You have selected: " + item)
+        print("Please enter the ranking you would like to assign to this item.")
+        print("1 is the highest ranking, 15 is the lowest.")
+        ranking = input()
+        if ranking in range(1,16) and ranking not in playerRankings:
+            playerRankings.append(ranking)
+        else:
+            print("Invalid ranking. Please enter a number between 1 and 15.")
+            rankItems()
+    else:
+        print("Invalid item. Please select an item from the list.")
+        clearScreen()
+        rankItems()
+    playerRankings.append(input("Rank " + str(i+1) + ": "))
+
+runGame()
